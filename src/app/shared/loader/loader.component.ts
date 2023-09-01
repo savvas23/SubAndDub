867261dac4ec393d7926d5225c74563bb6851ba5
Savvas Salampasis <savvassal@arx.net>
2023-09-01 16:50:57 +0300

    add loader, staging for timestamp validation

---
+++ src/app/shared/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sd-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+export class LoaderComponent {
+
+}
--- src/config.ts
+++
@@ -1,1 +0,0 @@
-export const GTRANSLATE_API_KEY = 'API_KEY'
