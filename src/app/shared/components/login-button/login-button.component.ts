84e09526f3076a5616d39f7efe8228377f2e6f13
Savvas Salampasis <savvassal@arx.net>
2023-09-08 17:12:07 +0300

    firebase auth and firestore integrated & more

---
+++ src/app/shared/components/login-button/login-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'login-button',
+  templateUrl: './login-button.component.html',
+  styleUrls: ['./login-button.component.css']
+})
+export class LoginButtonComponent {
+  
+  constructor(public auth: AuthService ) { }
+}
