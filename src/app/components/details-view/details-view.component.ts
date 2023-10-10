ab20be9fbf11c03a7aa3d79fe81079d195adaefd
Savvas Salampasis <savvassal@arx.net>
2023-10-10 20:57:20 +0300

    css changes

--- src/app/components/details-view/details-view.component.ts
+++ src/app/components/details-view/details-view.component.ts
@@ -14,12 +14,12 @@ export class DetailsViewComponent implements OnInit {
   videoId: string;
   videoDetails$: BehaviorSubject<YoutubeVideoDetails[]> = new BehaviorSubject<YoutubeVideoDetails[]>(null);
   loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
-  displayedColumns;
+  displayedColumns = ['Language', 'Last Updated', 'Subtitles'];
   dataSource;
   constructor(private route: ActivatedRoute, private router: Router,private youtubeService: YoutubeService, private detailsViewService: DetailsViewServiceService) { }
 
   ngOnInit(): void {
-    this.displayedColumns = ['Language', 'Last Updated', 'Subtitles'];
+
     this.dataSource = ELEMENT_DATA;
     this.videoId = this.route.snapshot.paramMap.get('id');
     this.youtubeService.getVideoDetails(this.videoId).pipe(
@@ -42,7 +42,7 @@ export class DetailsViewComponent implements OnInit {
 
 export const ELEMENT_DATA = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 2, name: 'ayo', weight: 4.0026, symbol: 'He'},
   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
